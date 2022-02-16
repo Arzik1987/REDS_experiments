8f1476e96fd3a90680356418d1ef609fd79966e4
Vadim Arzamasov <arzamasov@i40pool.ipd.kit.edu>
2022-02-16 23:37:18 +0100

    changed .bat with adding -f

--- main/get_friedman.bat
+++ main/get_friedman.bat
@@ -1,6 +1,6 @@
 git clone https://github.com/EpistasisLab/regression-benchmark.git
 cd regression-benchmark
-git checkout 07a71de3ed8e7009f08c5a21bcbf17276b0c946b
+git checkout -f 07a71de3ed8e7009f08c5a21bcbf17276b0c946b
 cd ..
 echo f | xcopy /s ".\regression-benchmark\postprocessing\friedman.r" ".\friedman.r"
 @RD /S /Q ".\regression-benchmark"
